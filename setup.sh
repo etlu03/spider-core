53792db2bd59738c307eecb192609fa80b128cca
etlu03 <etlu03@gmail.com>
2023-07-29 16:39:25 -0400

    chore: updated shell script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 echo "Installing core dependencies...";
 npm install puppeteer@19.7.5 > /dev/null;
-npm install fs@0.0.1-security > /dev/null;
-npm install process@0.11.10 > /dev/null;
-npm install path@0.12.7 > /dev/null;
 
-echo "Install development dependencies...";
+echo "Installing development dependencies...";
 npm install --save-dev eslint@latest > /dev/null;
 npm install --save-dev eslint-plugin-prettier@latest > /dev/null;
 npm install --save-dev prettier@latest > /dev/null;
